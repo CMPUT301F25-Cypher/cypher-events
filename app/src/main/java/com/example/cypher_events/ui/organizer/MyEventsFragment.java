a1ee1dd905d9cb3c9d7d43658a8ef6c869ddceb7
BetterThanYou73 <manas2005mj@gmail.com>
2025-11-07 07:35:41 -0700

    Entrant Organizer fully function, need to be made dynamic

--- app/src/main/java/com/example/cypher_events/ui/organizer/MyEventsFragment.java
+++ app/src/main/java/com/example/cypher_events/ui/organizer/MyEventsFragment.java
@@ -4,17 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import com.example.cypher_events.R;
 
 public class MyEventsFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_my_events, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_events, container, false);
+
+        ImageButton btnBack = view.findViewById(R.id.btnBack);
+        if (btnBack != null) {
+            btnBack.setOnClickListener(v ->
+                    requireActivity().getSupportFragmentManager()
+                            .beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.container, new OrganizerDashboardFragment())
+                            .commit());
+        }
+
+        return view;
     }
 }
